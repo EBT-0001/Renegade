0de4494be65b2c7bc44f999a03c345994a3a31e1
Temperlius <ethanbtemperley@outlook.com>
2026-08-31 10:47:29 -0500

    Update physics.c

--- src/physics.c
+++ src/physics.c
@@ -1,3 +1,10 @@
+/*
+	Renegade  Copyright (C) 2026  Temperlius
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+*/
+
 #include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
