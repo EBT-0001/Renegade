ccfe3b60187adb6c97dd1569b70e78910160ae42
Temperlius <ethanbtemperley@outlook.com>
2026-09-04 12:23:42 -0500

    Update enemy.cpp

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,6 +1,18 @@
+/*
+	Renegade  Copyright (C) 2026  Temperlius
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+*/
+
 #include <cstdint>
 
 #include "world.h"
+
+typedef struct {
+	void (function*)() = nullptr;
+	bool assigned;
+} enemyLogic;
 
 auto newEnemyLogic(bool chasePlayer, uint8_t detectDistance) {
 	if (chasePlayer) {
