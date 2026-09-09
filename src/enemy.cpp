bfa2704012c50e5f6d829d4f1f00ceb1964c3eec
Temperlius <ethanbtemperley@outlook.com>
2026-09-09 11:02:26 -0500

    Update enemy.cpp

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -7,20 +7,25 @@
 
 #include <cstdint>
 
+#include "functions.hpp"
+
 extern "C" {
 	#include "world.h"
 }
 
-typedef struct {
-	void (*function)() = nullptr;
-	bool assigned;
-} enemyLogic;
-
-auto newEnemyLogic(bool chasePlayer, uint8_t detectDistance) {
+void newEnemyFunction(uint8_t Enemy, bool chasePlayer, uint8_t detectDistance) {
 	if (chasePlayer) {
-		
+		*functions[Enemy].function = []() {
+			if (
+				World.entities[Enemy].transform->x < World.entities[0].transform->x + detectDistance &&
+				World.entities[Enemy].transform->x + detectDistance > World.entities[0].transform->x
+			) {
+				if (World.entities[0].transform->position.x < World.entities[Enemy].transform->position.x) {
+					World.entities[Enemy].transform->velocity.x -= *World.entities[Enemy].speed
+				} else if (World.entities[0].transform->position.x > World.entities[Enemy].transform->position.x) {
+					World.entities[Enemy].transform->velocity.x += *World.entities[Enemy].speed
+				}
+			}
+		};
 	}
-	return []() {
-		
-	};
 }
