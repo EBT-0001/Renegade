0e42eafec120dfb2d05041b5ce909aaa580b9120
Temperlius <ethanbtemperley@outlook.com>
2026-09-22 11:02:32 -0500

    Update test.lua

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,2 +1,12 @@
-x = getValue(0, "x");
-print(x)
+mouseX = getValue(nil, "mouseX");
+mouseY = getValue(nil, "mouseY");
+leftClick = getValue(nil, "leftClick");
+
+X = getValue(parent, "x");
+y = getValue(parent, "y");
+width = getValue(parent, "width");
+height = getValue(parent, "height");
+
+if () then
+	
+end
